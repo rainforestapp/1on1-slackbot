8147cef99213a9218ed937d82b20eead1fe72443
Jezen Thomas <jezenthomas@gmail.com>
2015-11-02 13:44:27 +0100

    Add tests for .atMention() method

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,10 +1,26 @@
 import {App} from '../src/app';
 
 describe('App', () => {
+  let app = new App();
+
   describe('.createPairs', () => {
     it('returns pairs', () => {
-      var app = new App();
       expect(app.createPairs()).toEqual([]);
+    });
+  });
+
+  describe('.atMention', () => {
+    describe('with a list of names', () => {
+      it('prefixes @ symbols to a list of names', () => {
+        let people = ['alice', 'bob'];
+        expect(app.atMention(people)).toEqual(['@alice', '@bob']);
+      });
+    });
+
+    describe('with an empty list', () => {
+      it('returns an empty list', () => {
+        expect(app.atMention([])).toEqual([]);
+      });
     });
   });
 });
